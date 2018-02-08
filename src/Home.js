39b21cf7b2631d506a3440fdd1583e5a97ef1248
Maximilian Ehlers <maximilian.ehlers@innovation.hrs.com>
2018-02-08 09:22:54 +0100

    initial commit

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import logo from "./react.svg";
+import styles from "./Home.module.css";
+import "./Home.css";
+
+class Home extends Component {
+  render() {
+    return <div className={styles.home} />;
+  }
+}
+
+export default Home;
